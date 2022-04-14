a611de4d0377afda7ddd15de9132b5d59dab75c6
Geo Angelopoulos <georgios@trality.com>
2022-04-14 22:33:55 +0200

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let slices: Vec<&str> = s.split(',').collect();
+        if slices.len() == 2 && !slices[0].is_empty() && !slices[1].is_empty() {
+            match slices[1].parse::<usize>() {
+                Ok(n) => Person {
+                    name: slices[0].to_string(),
+                    age: n,
+                },
+                Err(_) => Person::default(),
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
